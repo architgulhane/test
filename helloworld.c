bdb80d230259a1d1ab6569e5c16507f7d800e4d0
architgulhane <architgulhane6@gmail.com>
2024-08-12 20:01:26 +0530

    add print

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    a=10;
-    b=20;
-    c=a+b;
-    printf("The sum of no. is:");
-    scanf("%d",c);
+    printf("Hello World");
+    printf("hi there");
 
 }
