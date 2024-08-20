b0f93a17f238179e150f96414f3a0318fbb8f272
architgulhane <147231625+architgulhane@users.noreply.github.com>
2024-08-20 15:29:44 +0000

    javascript

---
+++ .devcontainer/java.js
@@ -0,0 +1,35 @@
+ //let v = "black"
+ //var p =  "blue"
+//Number,String,boolean
+//let v = null
+//console.log(typeof v)
+//let x = "69adc"
+//let b = Number(x)
+//console.log(typeof x)
+//console.log(typeof b) 
+
+//const id = Symbol("1,2,3")
+//const anotherid = Symbol("1,2,3")
+//if (id === anotherid ) {
+  //  console.log("x")
+//}
+//else ()
+
+
+// (string intercollaction)
+//let v = "  mulla  "
+//console.log(`hello lulla ${v.trim()}`); //${v} ,
+// ${v.trim()} -trim to remove extra space
+// use backtick ` and curl bracket 
+
+//let x = Math.floor()   // maths for random value
+//console.log(x); // floor= bottom value, ciel = upper value.
+/*
+let newDate = new Date()
+let betterdate = (newDate.toLocaleDateString()) // date and time
+console.log(betterdate);
+*/
+/*
+let timestamp = Date.now() //time stamp
+console.log(timestamp);
+*/
